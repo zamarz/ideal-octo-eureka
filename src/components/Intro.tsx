e55f77cb666ed026edbf086f8efdfc34cb420043
zamarz <zachmarzouk@gmail.com>
2024-02-19 15:20:19 +0000

    Add images as assets

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
+
 const Intro = () => {
   return (
-    <div>
-      <h2>My into will go here inshallah</h2>
-    </div>
+    <section>
+      <div className="text-center justify-center items-center py-10">
+        <Image
+          src="https://github.com/zamarz/ideal-octo-eureka/blob/main/public/assets/images/ZM_profile.jpg"
+          className="w-full sm:w-auto sm:max-w-lg mx-auto "
+          alt="A portrait image of the developer"
+          width={400}
+          height={400}
+        />
+        <h2 className="font-black text-2xl">Hi, I'm Zach</h2>
+        <p>
+          I'm an enthusiastic developer keen to delve deeper into the tech world
+          and continue my development journey.{" "}
+        </p>
+      </div>
+    </section>
   );
 };
 
