1f80a777247c407823e6e8377ae9729072135218
zamarz <zachmarzouk@gmail.com>
2024-02-19 14:27:19 +0000

    Adjust tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#038C7F",
+        secondary: "#F2C641",
+        tertiary: {
+          dark: "#F27405",
+          light: "#F2C641",
+        },
       },
     },
   },
