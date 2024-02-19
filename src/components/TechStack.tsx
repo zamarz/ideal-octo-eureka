332b9bbe3fa7c9275347d81136aeb1830b7217fe
zamarz <zachmarzouk@gmail.com>
2024-02-19 15:37:59 +0000

    Add tech stack placeholder

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,5 +1,15 @@
 const TechStack = () => {
-  return <div>TechStack goes here. Should be scrolling across page?</div>;
+  return (
+    <section>
+      <div className="text-center justify-center items-center py-10">
+        <div>
+          <h2 className="font-black text-2xl">My Tech Stack</h2>
+          <p>Here are some of the technologies I use...</p>
+          <p>TechStack goes here. Should be scrolling across page?</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TechStack;
