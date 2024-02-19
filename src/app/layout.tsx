b5ab40c04fd42d5029e168810ba1a596e9fb8700
zamarz <zachmarzouk@gmail.com>
2024-02-19 14:27:46 +0000

    Alter components folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ThemeProvider from "@/Components/ThemeProvider";
+import ThemeProvider from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
