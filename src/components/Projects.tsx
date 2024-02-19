9ecdea7bf1af025f1eadceb6edc1c250695b2b05
zamarz <zachmarzouk@gmail.com>
2024-02-19 14:59:53 +0000

    Add dark mode to tailwind config

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,10 @@
 const Projects = () => {
-  return <div>My recent projects goes here</div>;
+  return (
+    <div>
+      My recent projects goes here. Connect to Firebase. See if analytics
+      possible
+    </div>
+  );
 };
 
 export default Projects;
