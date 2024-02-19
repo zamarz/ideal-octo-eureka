ff4cc01465c3ca415262ecf8758f4123dec04d9b
zamarz <zachmarzouk@gmail.com>
2024-02-19 15:56:48 +0000

    Add placeholder for projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,15 @@
 const Projects = () => {
   return (
-    <div>
-      My recent projects goes here. Connect to Firebase. See if analytics
-      possible
-    </div>
+    <section>
+      <div className="text-center justify-center items-center py-10">
+        <h2 className="font-black text-2xl">My projects</h2>
+        <p>
+          {" "}
+          My recent projects goes here. Connect to Firebase. See if analytics
+          possible
+        </p>
+      </div>
+    </section>
   );
 };
 
