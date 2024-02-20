13c92f009cccdb0dc5e00fc19ac90a73f02295c2
zamarz <zachmarzouk@gmail.com>
2024-02-20 10:34:19 +0000

    Center about me section in container

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,20 +1,24 @@
 const AboutMe = () => {
   return (
-    <section>
-      <div className="text-center justify-center items-center py-10 ">
-        <h2 className="font-black text-2xl">About Me</h2>
-        <div className="py-3 text-wrap">
-          <p className="mx-12 px-12">
+    <section className="px-10 py-10 mx-auto container">
+      <div className="py-10 mx-10 px-10">
+        <h2 className="font-black text-3xl">About Me</h2>
+        <div className="py-3 text-xl">
+          <p className="">
             {" "}
             After covering the world of B2B tech as a writer, I decided to take
             the plunge and dive even deeper into the industry by becoming a
             software developer.{" "}
           </p>
           <br></br>
-          <p className="mx-12 px-12">
+          <p className="">
             This is something that has always fascinated me and I find it very
             satisfying to connect the pieces of the puzzle and understand how
             everything works, not only in the back-end but the front-end too.
+          </p>
+          <br></br>
+          <p>
+            {" "}
             I'm always looking to learn new things and jump at the opportunity
             to try my hand at unfamiliar technologies.
           </p>
