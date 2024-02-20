52ee6fc8097cd13ed2ab9be98bef7b4d7406bcce
zamarz <zachmarzouk@gmail.com>
2024-02-20 09:16:23 +0000

    Add more styling to About Me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,16 +3,22 @@ const AboutMe = () => {
     <section>
       <div className="text-center justify-center items-center py-10 ">
         <h2 className="font-black text-2xl">About Me</h2>
-        <p className="mx-12 px-12 py-6 ">
-          {" "}
-          After covering the world of B2B tech as a writer, I decided to take
-          the plunge and dive even deeper into the industry by becoming a
-          software developer. This is something that has always fascinated me
-          and I find it very satisfying to connect the pieces of the puzzle and
-          understand how everything works, not only in the back-end but the
-          front-end too. I'm always looking to learn new things and jump at the
-          opportunity to try my hand at unfamiliar technologies.{" "}
-        </p>
+        <div className="py-3 text-wrap">
+          <p className="mx-12 px-12">
+            {" "}
+            After covering the world of B2B tech as a writer, I decided to take
+            the plunge and dive even deeper into the industry by becoming a
+            software developer.{" "}
+          </p>
+          <br></br>
+          <p className="mx-12 px-12">
+            This is something that has always fascinated me and I find it very
+            satisfying to connect the pieces of the puzzle and understand how
+            everything works, not only in the back-end but the front-end too.
+            I'm always looking to learn new things and jump at the opportunity
+            to try my hand at unfamiliar technologies.
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,14 +6,14 @@ const Intro = () => {
       <div className="text-center justify-center items-center py-10">
         <Image
           src={
-            "https://raw.githubusercontent.com/zamarz/ideal-octo-eureka/main/public/assets/images/ZM_profile.jpg"
+            "https://raw.githubusercontent.com/zamarz/cuddly-guide/main/assets/images/ZM_profile.jpg"
           }
           className="w-full sm:w-auto sm:max-w-lg mx-auto rounded-full"
           alt="A portrait image of the developer"
           width={300}
           height={400}
         />
-        <h2 className="font-black text-2xl">Hi, I'm Zach</h2>
+        <h2 className="font-black text-2xl py-5">Hi, I'm Zach</h2>
         <p>
           I'm an enthusiastic developer keen to delve deeper into the tech world
           and continue my development journey.{" "}
