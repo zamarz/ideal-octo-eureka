f5adf3cc756c85c6fb3d7e9b5e893e5f0be28464
zamarz <zachmarzouk@gmail.com>
2024-02-20 11:10:43 +0000

    Add newi image

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -89,11 +89,11 @@ const TechStack = () => {
           <div className="flex flex-col items-center py-5">
             <Image
               src={
-                "https://raw.githubusercontent.com/zamarz/ideal-octo-eureka/main/public/assets/icons/icons8-typescript-96.png"
+                "https://raw.githubusercontent.com/zamarz/ideal-octo-eureka/main/public/assets/icons/icons8-typescript-56.png"
               }
               alt="The TypeScript Logo"
-              width={200}
-              height={200}
+              width={100}
+              height={100}
             />
             <h2 className="text-xl">TypeScript</h2>
           </div>
