d28b38e9e5b01acb25de566db8d68a49b1e894f4
zamarz <zachmarzouk@gmail.com>
2024-02-20 11:48:54 +0000

    Add pizza icon to intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { IoPizzaOutline } from "react-icons/io5";
 
 const Intro = () => {
+  const style = { fontSize: "2em" };
+
   return (
     <section className="px-10 py-10 mx-auto container min-h-screen">
-      <div className="text-center justify-center items-center py-10">
+      <div className=" flex flex-col text-center justify-center items-center py-10">
         <Image
           src={
             "https://raw.githubusercontent.com/zamarz/cuddly-guide/main/assets/images/ZM_profile.jpg"
@@ -13,7 +16,12 @@ const Intro = () => {
           width={300}
           height={400}
         />
-        <h2 className="font-black text-4xl py-5">Hi, I'm Zach</h2>
+        <div className="flex flex-row items-center py-5 ">
+          <h2 className="font-black text-4xl">Hi, I'm Zach</h2>
+          <div className="px-5">
+            <IoPizzaOutline style={style} />
+          </div>
+        </div>
         <p className="text-xl">
           I'm an enthusiastic developer keen to delve deeper into the tech world
           and continue my development journey.{" "}
