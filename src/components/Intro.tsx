e71b59cf9b792b33186007a672b851ca27d70fb7
zamarz <zachmarzouk@gmail.com>
2024-02-20 14:21:01 +0000

    Change image style in intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,15 +7,16 @@ const Intro = () => {
   return (
     <section className="px-10 py-10 mx-auto container min-h-screen">
       <div className=" flex flex-col text-center justify-center items-center py-10">
-        <Image
-          src={
-            "https://raw.githubusercontent.com/zamarz/cuddly-guide/main/assets/images/ZM_profile.jpg"
-          }
-          className="w-full sm:w-auto sm:max-w-lg mx-auto rounded-full"
-          alt="A portrait image of the developer"
-          width={300}
-          height={400}
-        />
+        <div className="h-64 w-96 relative">
+          <Image
+            src={"/assets/images/ZM_profile.jpg"}
+            className="w-full h-auto sm:w-auto sm:max-w-lg mx-auto rounded-lg"
+            alt="A portrait image of the developer"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
         <div className="flex flex-row items-center py-5 ">
           <h2 className="font-black text-4xl">Hi, I'm Zach</h2>
           <div className="px-5">
