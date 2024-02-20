5936fee08adcb53766edd851d10b9d8ca030a9f8
zamarz <zachmarzouk@gmail.com>
2024-02-20 13:16:52 +0000

    Populate Footer with more information and style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
 
 const Footer = () => {
   return (
@@ -8,9 +9,32 @@ const Footer = () => {
           <Link href="/" className="font-black text-tertiary-dark">
             Zach Marzouk
           </Link>
+          <div className="space-y-2">
+            <h4 className="font-semibold mb-4">Links</h4>
+            <div className="flex items-center space-x-2">
+              <AiFillGithub />
+              <Link href={"https://github.com/zamarz"}>GitHub</Link>
+            </div>
+            <div className="flex items-center space-x-2">
+              <AiFillLinkedin />
+              <Link href={"https://www.linkedin.com/in/zachmarzouk/"}>
+                LinkedIn
+              </Link>
+            </div>
+          </div>
+          <div className="space-y-2">
+            <h4 className="font-semibold mb-4">Contact Me</h4>
+            <div className="flex items-center space-x-2">
+              <AiOutlineMail />
+              <Link href={"mailto:zmarzouk23@gmail.com"}>Send an email</Link>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>ICONS 8 LINKEDIN GITHUB MAIN PAGE</p>
+        <div className="mt-8 text-center text-sm">
+          <p>&copy; {new Date().getFullYear()} Zach Marzouk</p>
+          <p>
+            Icons by <a href="https://icons8.com/">Icons8</a>
+          </p>
         </div>
       </div>
     </footer>
