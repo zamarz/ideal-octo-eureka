8c3ec0f8685715980648b74fc124beab28cd4acd
zamarz <zachmarzouk@gmail.com>
2024-02-20 14:08:06 +0000

    Changed styling in articles section to fit images

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -11,17 +11,17 @@ const Articles = () => {
         <p className="text-xl py-3">A selection of articles I have written</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 text-center justify-center items-center mx-4  py-10 my-10">
-        <div>
-          <Image
-            src={
-              "https://raw.githubusercontent.com/zamarz/ideal-octo-eureka/main/public/assets/images/Japan_image_unsplash.jpg"
-            }
-            width={200}
-            height={200}
-            alt="An image of a mountain in Japan"
-            className="w-full sm:w-auto sm:max-w-lg mx-auto my-4"
-          />
-          <h2 className="font-black text-2xl my-2">
+        <div className="flex flex-col items-center">
+          <div className="h-64 w-96 relative">
+            <Image
+              src={"/assets/images/Japan_image_unsplash.jpg"}
+              layout="fill"
+              objectFit="cover"
+              alt="An image of a mountain in Japan"
+              className="w-full h-auto sm:w-auto sm:max-w-lg mx-auto my-4"
+            />
+          </div>
+          <h2 className="font-black text-2xl my-5">
             Why does Japan lag behind on startups?
           </h2>
           <p className="px-3 mx-3 my-3">
@@ -38,17 +38,17 @@ const Articles = () => {
             </a>
           </button>
         </div>
-        <div>
-          <Image
-            src={
-              "https://raw.githubusercontent.com/zamarz/ideal-octo-eureka/main/public/assets/images/Typing_Unsplash.jpg"
-            }
-            width={200}
-            height={200}
-            alt="An image of code on a laptop screen"
-            className="w-full sm:w-auto sm:max-w-lg mx-auto my-4"
-          />
-          <h2 className="font-black text-2xl my-2">
+        <div className="flex flex-col items-center">
+          <div className="h-64 w-96 relative">
+            <Image
+              src={"/assets/images/Typing_Unsplash.jpg"}
+              layout="fill"
+              objectFit="cover"
+              alt="An image of code on a laptop screen"
+              className="w-full sm:w-auto sm:max-w-lg mx-auto my-4"
+            />
+          </div>
+          <h2 className="font-black text-2xl my-5">
             With AI on the rise, is it time to join a union?
           </h2>
           <p className="px-3 mx-3 my-3">
@@ -65,17 +65,17 @@ const Articles = () => {
             </a>
           </button>
         </div>
-        <div>
-          <Image
-            src={
-              "https://raw.githubusercontent.com/zamarz/ideal-octo-eureka/main/public/assets/images/imgix_data_centre.jpg"
-            }
-            width={200}
-            height={200}
-            alt="An image of a mountain in Japan"
-            className="w-full sm:w-auto sm:max-w-lg mx-auto my-4"
-          />
-          <h2 className="font-black text-2xl my-2">
+        <div className="flex flex-col items-center">
+          <div className="h-64 w-96 relative">
+            <Image
+              src={"/assets/images/imgix_data_centre.jpg"}
+              layout="fill"
+              objectFit="cover"
+              alt="An image of data centre servers"
+              className="w-full sm:w-auto sm:max-w-lg mx-auto my-4"
+            />
+          </div>
+          <h2 className="font-black text-2xl my-5">
             Why Frankfurt is clamping down on data center sprawl
           </h2>
           <p className="px-3 mx-3 my-3">
