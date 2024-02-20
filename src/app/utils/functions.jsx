ef4c224a53bd96f85c0e7c7329545f845a6a7c6e
zamarz <zachmarzouk@gmail.com>
2024-02-20 09:53:58 +0000

    Add visibility effect to home page

---
+++ src/app/utils/functions.jsx
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function useIsVisible(ref) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    });
+
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRef } from "react";
+import { useIsVisible } from "../app/utils/functions";
 import AboutMe from "./AboutMe";
 import Articles from "./Articles";
 import Intro from "./Intro";
@@ -5,14 +9,60 @@ import Projects from "./Projects";
 import TechStack from "./TechStack";
 
 const HomePage = () => {
+  const ref1 = useRef<HTMLDivElement>(null);
+  const isVisible = useIsVisible(ref1);
+  const ref2 = useRef<HTMLDivElement>(null);
+  const isVisible2 = useIsVisible(ref2);
+  const ref3 = useRef<HTMLDivElement>(null);
+  const isVisible3 = useIsVisible(ref3);
+  const ref4 = useRef<HTMLDivElement>(null);
+  const isVisible4 = useIsVisible(ref4);
+  const ref5 = useRef<HTMLDivElement>(null);
+  const isVisible5 = useIsVisible(ref5);
+
   return (
-    <>
-      <Intro />
-      <AboutMe />
-      <TechStack />
-      <Projects />
-      <Articles />
-    </>
+    <div>
+      <div
+        ref={ref1}
+        className={`transition-opacity ease-in duration-700  ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <Intro />
+      </div>
+      <div
+        ref={ref2}
+        className={`transition-opacity ease-in duration-700  ${
+          isVisible2 ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <AboutMe />
+      </div>
+      <div
+        ref={ref3}
+        className={`transition-opacity ease-in duration-700  ${
+          isVisible3 ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <TechStack />
+      </div>
+      <div
+        ref={ref4}
+        className={`transition-opacity ease-in duration-700  ${
+          isVisible4 ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <Projects />
+      </div>
+      <div
+        ref={ref5}
+        className={`transition-opacity ease-in duration-700  ${
+          isVisible5 ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <Articles />
+      </div>
+    </div>
   );
 };
 
